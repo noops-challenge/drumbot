311367cb3d1ad471270d0f06c2a398330c9d5a7b
Dave Foley <davidmfoley@gmail.com>
2019-06-11 09:58:22 -0700

    Updates

---
+++ precache-manifest.c047f86c9d7416c602c3b8ff7f5aba2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ef59b8afcbad461ba8328a7d6e165c1",
+    "url": "/drumbot/index.html"
+  },
+  {
+    "revision": "f8bcd1a8708b1bdfe4b1",
+    "url": "/drumbot/static/css/main.9dd2b164.chunk.css"
+  },
+  {
+    "revision": "3cf1e96bba0e046e8298",
+    "url": "/drumbot/static/js/2.302ee91e.chunk.js"
+  },
+  {
+    "revision": "f8bcd1a8708b1bdfe4b1",
+    "url": "/drumbot/static/js/main.53198c70.chunk.js"
+  },
+  {
+    "revision": "e920a53419301ebe86ab",
+    "url": "/drumbot/static/js/runtime~main.5f960664.js"
+  }
+]);
