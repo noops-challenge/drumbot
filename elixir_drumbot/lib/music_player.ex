14b622206c9821ec9910a1f1836c3dd8750e1f23
Carlo Gilmar <carlogilmar12@gmail.com>
2019-06-17 10:36:32 -0500

    Adding basic counter

--- elixir_drumbot/lib/music_player.ex
+++ elixir_drumbot/lib/music_player.ex
@@ -2,26 +2,34 @@ defmodule Drumbot.MusicPlayer do
   use GenServer
 
   def start_link(), do: GenServer.start_link(__MODULE__, [], [name: __MODULE__])
-  def init(_), do: {:ok, {:general_score, 0} }
+  def init(max_time) do
+    IO.inspect max_time
+    {:ok, {:timer, 10, 0} }
+  end
 
   def assert(), do: GenServer.cast( __MODULE__, {:assert} )
-  def fail(), do: GenServer.cast( __MODULE__, {:fail} )
+
+  def loop(), do: send self(), :loop
+  def handle_info(:loop, state) do
+    {:timer, max_time, current} = state
+    validate_max_time = fn
+      true ->
+        IO.puts "DONE!!"
+        {:noreply, state}
+      false ->
+        :timer.sleep 1000
+        loop()
+        IO.puts "Max time: #{max_time} Seg: #{current}"
+        {:noreply, {:timer, max_time, current+1} }
+    end
+    validate_max_time.(max_time==current)
+  end
 
   def handle_cast( {:assert}, state) do
-    {_, score} = state
-    next_score = score+1
-    IO.puts "Score: #{next_score}"
-    {:noreply, {:general_score, next_score} }
+    IO.puts "Empezando"
+    IO.inspect state
+    loop()
+    {:noreply, state}
   end
-
-  def handle_cast( {:fail}, state) do
-    {_, score} = state
-    next_score = decrement_score( score )
-    IO.puts "Score: #{next_score}"
-    {:noreply, {:general_score, next_score} }
-  end
-
-  def decrement_score( 0 ), do: 0
-  def decrement_score( score ), do: score-1
 
 end
