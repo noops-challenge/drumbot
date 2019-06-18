20bd28764886b897688006d78a1c6a2ff7b24d4f
Carlo Gilmar <carlogilmar12@gmail.com>
2019-06-18 00:25:27 -0500

    Adding styles

--- elixir_drumbot/lib/music_player.ex
+++ elixir_drumbot/lib/music_player.ex
@@ -24,20 +24,37 @@ defmodule Drumbot.MusicPlayer do
 				next_tracks_states = for {_instrument, track_state} <- tracks_to_play, do: track_state
 				states_for_monitor = play_sound(previous_tracks_states, tracks_to_play)
         IO.puts "- - - - - - - - - - - - - - - - - - - - - "
-        [:color201, "     .:: GitHub Meet the Noobs ::. \n"] |> Bunt.puts
-        [:color154, "######  DJ Drumbot _             /#####"] |> Bunt.puts
-        [:color154, "#( )# |          _( )__         | #( )#"] |> Bunt.puts
-        [:color154, "##### |         /_    /         | #####"] |> Bunt.puts
-        [:color154, "#   # |     ___m/I_ //_____     | #   #"] |> Bunt.puts
-        [:color154, "# O # |____#-x.\ /++m\ /.x-#____| # O #"] |> Bunt.puts
-        [:color154, "#m.m# |   // \ ///###\\\ / /\   | #m.m#"] |> Bunt.puts
-        [:color154, "#####/    ######/     \######    \#####"] |> Bunt.puts
+        print_dj(current)
 				IO.puts "[#{current}]/[#{song.duration}]"
         states_for_monitor |> Bunt.puts
         IO.puts "- - - - - - - - - - - - - - - - - - - - - "
         {:noreply, {current+1, index+1, song, next_tracks_states}}
     end
     validate_max_time.(song.duration==current)
+  end
+
+  def print_dj(current) do
+    show_dj = fn
+      0 ->
+        [:color201, "     .:: GitHub Meet the Noobs ::. \n"] |> Bunt.puts
+        [:color154, "######  DJ Drumbot               /#####"] |> Bunt.puts
+        [:color154, "#( )# |          _ 🧐__          | #( )#"] |> Bunt.puts
+        [:color154, "##### |         /_    /         | #####"] |> Bunt.puts
+        [:color154, "#   # |     ___m/I_ //_____     | #   #"] |> Bunt.puts
+        [:color154, "# O # |____#-x.\ /++m\ /.x-#____| # O #"] |> Bunt.puts
+        [:color154, "#m.m# |   // \ ///###\\\ / /\   | #m.m#"] |> Bunt.puts
+        [:color154, "#####/    ######/     \######    \#####"] |> Bunt.puts
+      _ ->
+        [:color201, "     .:: GitHub Meet the Noobs ::. \n"] |> Bunt.puts
+        [:color154, "######  DJ Drumbot     🤘🏻        /######"] |> Bunt.puts
+        [:color154, "#( )# |          _ 😌__||       | #( )#"] |> Bunt.puts
+        [:color154, "##### |         /_   |          | #####"] |> Bunt.puts
+        [:color154, "#   # |     ___m/I_  |_____     | #   #"] |> Bunt.puts
+        [:color154, "# O # |____#-x.\ /++m\ /.x-#____| # O #"] |> Bunt.puts
+        [:color154, "#m.m# |   // \ ///###\\\ / /\   | #m.m#"] |> Bunt.puts
+        [:color154, "#####/    ######/     \######    \#####"] |> Bunt.puts
+    end
+    show_dj.(rem(current,2))
   end
 
 	def play_sound(previous_states, tracks_to_play) do
