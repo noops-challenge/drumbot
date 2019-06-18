27fac8afd6e11449c596f0689c90548d8d287408
Carlo Gilmar <carlogilmar12@gmail.com>
2019-06-17 23:34:33 -0500

    Replacing instrument by my personal sounds

--- elixir_drumbot/lib/util.ex
+++ elixir_drumbot/lib/util.ex
@@ -2,6 +2,7 @@ defmodule Drumbot.Util do
 	alias Drumbot.Song
 
   @base_url "https://api.noopschallenge.com/drumbot/patterns"
+  @instruments ["A.mp3", "B.mp3", "C.mp3", "E.mp3", "D.mp3"]
 
   def get(uri) do
     {:ok, response} = HTTPoison.get("#{@base_url}#{uri}")
@@ -9,12 +10,20 @@ defmodule Drumbot.Util do
   end
 
 	def build_model(song) do
+		tracks = replace_instruments(song["tracks"])
 		%Song{
 			name: song["name"],
 			steps: song["stepCount"],
 			duration: song["beatsPerMinute"],
-			tracks: song["tracks"]
+			tracks: tracks
 		}
+	end
+
+	def replace_instruments(tracks) do
+		new_tracks = Enum.zip(tracks, @instruments)
+		for {%{"instrument" => _instrument, "steps" => steps}, instrument} <- new_tracks do
+			%{"instrument" => instrument, "steps" => steps}
+		end
 	end
 
 	def get_starter_states(song) do
