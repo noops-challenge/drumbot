09f7147c6c35f1eefddf38fc9c6d1775d0bda011
Carlo Gilmar <carlogilmar12@gmail.com>
2019-06-17 21:18:09 -0500

    Calculating index in loop

--- elixir_drumbot/lib/music_player.ex
+++ elixir_drumbot/lib/music_player.ex
@@ -1,7 +1,6 @@
 defmodule Drumbot.MusicPlayer do
   use GenServer
 
-  #{:timer, 10, 0}
   def start_link(state \\ []), do: GenServer.start_link(__MODULE__, state, name: __MODULE__)
 	def init(state), do: {:ok, state}
 
@@ -9,25 +8,41 @@ defmodule Drumbot.MusicPlayer do
   def loop(), do: send self(), :loop
 
   def handle_info(:loop, state) do
-		{current, song} = state
-		max_time = song.duration
+		{current, current_index, song} = state
     validate_max_time = fn
       true ->
-        IO.puts "DONE!!"
+        IO.puts "Song Finished!!!!"
         {:noreply, state}
       false ->
         :timer.sleep 1000
         loop()
-        IO.puts "Max time: #{max_time} Seg: #{current}"
-        {:noreply, {current+1, song}}
+				index = get_index(current_index, song.steps)
+				_tracks_to_play = play_tracks(index, song.tracks)
+				IO.puts "[#{current}]/[#{song.duration}] [#{index}]"
+        {:noreply, {current+1, index+1, song}}
     end
-    validate_max_time.(max_time==current)
+    validate_max_time.(song.duration==current)
   end
 
   def handle_cast( {:play}, state) do
     loop()
     {:noreply, state}
   end
+
+	defp get_index(current, max_steps) do
+		loop_index = fn
+			true -> current
+			false -> 0
+		end
+		loop_index.(current<max_steps)
+	end
+
+	defp play_tracks(index, tracks) do
+		for %{"instrument" => instrument, "steps" => steps} <- tracks do
+			current_note = Enum.at(steps, index)
+			{instrument, current_note}
+		end
+	end
 
 end
 
